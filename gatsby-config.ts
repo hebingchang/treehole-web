2aade64cd040a61bff82940051f5c1a68fe178ba
hebingchang <hebingchang1@live.com>
2022-03-22 01:09:23 +0800

    Initial commit from gatsby

---
+++ gatsby-config.ts
@@ -0,0 +1,11 @@
+import type { GatsbyConfig } from "gatsby"
+
+const config: GatsbyConfig = {
+  siteMetadata: {
+    title: `treehole-web`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
+
+export default config
