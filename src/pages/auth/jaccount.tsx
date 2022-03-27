1a4f026a2454c4156295fe2bf61db0d071c176ec
hebingchang <hebingchang1@live.com>
2022-03-27 15:25:04 +0800

    feat: add auth fall back

--- src/pages/auth/jaccount.tsx
+++ src/pages/auth/jaccount.tsx
@@ -1,20 +1,43 @@
-import { navigate } from 'gatsby'
+import { Center, Spinner } from '@chakra-ui/react'
+import Cookies from 'js-cookie'
 import * as React from 'react'
 import { useEffect } from 'react'
+import { Helmet } from 'react-helmet'
 import { StringParam, useQueryParam } from 'use-query-params'
+import { LoginSource, OAuthLoginChannel } from '../../services/auth_pb'
+import rpc from '../../services/rpc'
+import { OAuthLoginRequest } from '../../services/treehole_pb'
+import { getWebSource } from '../../utils/env'
 
 const JAAuthPage = () => {
   const [code, _] = useQueryParam('code', StringParam)
 
   useEffect(() => {
     if (window.opener) window.opener.postMessage(code, '*')
-    else navigate('/')
+    else {
+      rpc.client
+        .oAuthLogin(
+          new OAuthLoginRequest()
+            .setChannel(OAuthLoginChannel.LOGINWITHJACCOUNT)
+            .setSource(LoginSource.LOGINSOURCEWEB)
+            .setWebSource(getWebSource())
+            .setCode(code!),
+          {}
+        )
+        .then((res) => {
+          Cookies.set('treehole_session', res.getToken())
+        })
+        .finally(() => (window.location.href = '/'))
+    }
   }, [])
 
   return (
-    <main>
-      <title>使用 jAccount 登录</title>
-    </main>
+    <Center h={`${window.innerHeight}px`} w='100%'>
+      <Helmet>
+        <title>使用 jAccount 登录</title>
+      </Helmet>
+      <Spinner />
+    </Center>
   )
 }
 
