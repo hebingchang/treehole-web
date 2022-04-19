ece1cacec18cbfb5c46dd6aa28c6e09344479dd3
hebingchang <hebingchang1@live.com>
2022-04-20 01:31:05 +0800

    feat: add vercel serverless function

---
+++ api/model.TreeHole/[method].ts
@@ -0,0 +1,15 @@
+import type { VercelRequest, VercelResponse } from "@vercel/node"
+
+const { createProxyMiddleware } = require("http-proxy-middleware")
+
+const apiProxy = createProxyMiddleware({
+  target: process.env.PROXY_URL,
+  changeOrigin: true,
+  pathRewrite: {
+    "^/api": ""
+  }
+})
+
+export default function handler(req: VercelRequest, res: VercelResponse) {
+  return apiProxy(req, res)
+}
