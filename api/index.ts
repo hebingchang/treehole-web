dca92a98900217afdc718400bb524a623d572142
hebingchang <hebingchang1@live.com>
2022-04-20 01:17:15 +0800

    feat: add vercel serverless function

--- api/index.ts
+++ api/index.ts
@@ -1,4 +1,4 @@
-import type { VercelRequest, VercelResponse } from "@vercel/node"
+import type { VercelRequest, VercelResponse, VercelApiHandler } from "@vercel/node"
 
 const { createProxyMiddleware } = require("http-proxy-middleware")
 
@@ -10,6 +10,22 @@ const apiProxy = createProxyMiddleware({
   }
 })
 
-export default function(req: VercelRequest, res: VercelResponse) {
+const allowCors = (fn: VercelApiHandler) => async (req: VercelRequest, res: VercelResponse) => {
+  res.setHeader("Access-Control-Allow-Origin", "*")
+  res.setHeader("Access-Control-Allow-Methods", "GET,OPTIONS,PATCH,DELETE,POST,PUT")
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
+  )
+  if (req.method === "OPTIONS") {
+    res.status(200).end()
+    return
+  }
+  fn(req, res)
+}
+
+const handler = (req: VercelRequest, res: VercelResponse) => {
   return apiProxy(req, res)
 }
+
+module.exports = allowCors(handler)
